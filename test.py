8cec726496f017b07f786ab901aae02ede3ff4e3
dprofili <dprofili@gmail.com>
2018-10-19 23:17:38 -0400

    Fix all justins shitty garbage

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
-def test():
-    chatbot = ChatBot('Will Li')
-    chatbot.set_trainer(ChatterBotCorpusTrainer)
-    chatbot.train("chatterbot.corpus.english")
-    chatterbot.set_trainer(ListTrainer)
+chatbot = ChatBot('Will Li')
+chatbot.set_trainer(ChatterBotCorpusTrainer)
+#chatbot.train("chatterbot.corpus.english")
+chatbot.set_trainer(ListTrainer)
 
-    fh = open('sample_data.txt')
-    lines = [line.rstrip('\n') for line in fh]
-    fh.close()
+fh = open('sample_data.txt')
+lines = [line.rstrip('\n') for line in fh]
+fh.close()
 
-    for x in range(0,5):
-        chatterbot.train(lines[x,x+1])
+for x in range(0,5):
+    chatbot.train(lines[x:x+1])
 
-    print chatbot.getresponse('How can I see my test?')
+print(chatbot.get_response('How can I see my test?'))
